e0ea72d996ad315a723327e845091a567f33212e
JohnLoftin <jgloftin@gmail.com>
2021-01-08 15:15:38 -0500

    moved lost/found links around name, created login button, additional styling for placement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 class Navbar extends Component {
     render() {
         return (
             <div className="navbar">
                 <div className="logo">
-                    <img className="logo-image" src="" />
+                    <img className="logo-image" src="./img/pets-logo.png" />
                 </div>
                 <div className="nav-name">
+                    <a className="lost" href="#">Lost Pets</a>
                     <h1>Pet-Finder</h1>
+                    <a href="#">Found Pets</a>
                 </div>
-                <div className="link-group">
-                    <ul className="links">
-                        <li><a href="#">Login</a></li>
-                        <li><a href="#">Lost Pets</a></li>
-                        <li><a href="#">Found Pets</a></li>
-                    </ul>
-                </div>
+                {/* <div className="login-button">
+                </div> */}
+                <button className="ui button">Login</button>
+
             </div>
         )
     }
