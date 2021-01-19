0cb88d09164afec8f57c902515a088aa41f39662
bensvarnum <bens.varnum@gmail.com>
2021-01-18 21:06:09 -0500

    add petForm to form folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Navbar, Footer } from "./components";
+import { Navbar, Footer, PetForm } from "./components";
 import { Landing } from "./pages";
+
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import "antd/dist/antd.css";
@@ -14,9 +15,9 @@ function App() {
         <Landing />
         <PetForm />
       </body>
-      <footer>
+      {/* <footer>
         <Footer />
-      </footer>
+      </footer> */}
     </div>
   );
 }
