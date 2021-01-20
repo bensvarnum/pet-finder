5be757de1f5f5d0295d7dad85f22c6ee4dc63876
bensvarnum <bens.varnum@gmail.com>
2021-01-19 20:55:52 -0500

    fix errors for build

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Image, Button } from "semantic-ui-react";
+import { Card, Image } from "semantic-ui-react";
 import styled from "styled-components";
 import ContactModal from "./ContactModal";
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,59 +1,77 @@
-import React, { useState } from 'react';
-import { Drawer, Button } from 'antd';
-import './navbar.css';
+import React, { useState } from "react";
+import { Drawer, Button } from "antd";
+import "./navbar.css";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
 
-    const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
 
-    const showDrawer = () => {
-        setVisible(true);
-    }
+  const onClose = () => {
+    setVisible(false);
+  };
 
-    const onClose = () => {
-        setVisible(false);
-    }
-
-    return (
-        <>
-        <div className="navbar" id="navbar">
-            <div className="logo">
-                <img className="logo-image" src="./img/pets-logo.png" />
-            </div>
-            <div className="nav-name">
-                <a className="lost" href="#">Lost Pets</a>
-                <img className="center-logo" src="./img/dog-bone-logo.png" />
-                <a className="found" href="#">Found Pets</a>
-            </div>
-            <div className="login-button">
-                <button className="fa fa-bars fa-2x" onClick={showDrawer}>
-                </button>
-            </div>
+  return (
+    <>
+      <div className="navbar" id="navbar">
+        <div className="logo">
+          <img
+            className="logo-image"
+            src="./img/pets-logo.png"
+            alt="pet logo"
+          />
         </div>
-        <Drawer
-            title="Navigation"
-            placement="left"
-            closable={true}
-            onClose={onClose}
-            visible={visible}
-        >
-            <div className="drawer-nav-links">
-                <h3>Search Categories</h3>
-                <div className="drawer-nav-link-group">
-                    <a className="lost" href="#">Lost Pets</a>
-                    <a className="found" href="#">Found Pets</a>
-                </div>
-            </div>
-            <div className="drawer-login-btn-group">
-                <h3>Login</h3>
-                <ul>
-                    <li><Button type="primary">Facebook</Button></li>
-                    <li><Button type="primary">Google</Button></li>
-                </ul>
-            </div>
-        </Drawer>
-        </>
-    )
-}
+        <div className="nav-name">
+          <a className="lost" href="/">
+            Lost Pets
+          </a>
+          <img
+            className="center-logo"
+            src="./img/dog-bone-logo.png"
+            alt="dog bone"
+          />
+          <a className="found" href="/">
+            Found Pets
+          </a>
+        </div>
+        <div className="login-button">
+          <button className="fa fa-bars fa-2x" onClick={showDrawer}></button>
+        </div>
+      </div>
+      <Drawer
+        title="Navigation"
+        placement="left"
+        closable={true}
+        onClose={onClose}
+        visible={visible}
+      >
+        <div className="drawer-nav-links">
+          <h3>Search Categories</h3>
+          <div className="drawer-nav-link-group">
+            <a className="lost" href="/">
+              Lost Pets
+            </a>
+            <a className="found" href="/">
+              Found Pets
+            </a>
+          </div>
+        </div>
+        <div className="drawer-login-btn-group">
+          <h3>Login</h3>
+          <ul>
+            <li>
+              <Button type="primary">Facebook</Button>
+            </li>
+            <li>
+              <Button type="primary">Google</Button>
+            </li>
+          </ul>
+        </div>
+      </Drawer>
+    </>
+  );
+};
 
 export default Navbar;
